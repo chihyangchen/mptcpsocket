68bce4a2719660560e49bb46f08b93bea3cad924
JingYou <ken32293355@gmail.com>
2022-05-03 22:37:56 +0800

    .

--- mptcp_auto_server.py
+++ mptcp_auto_server.py
@@ -36,40 +36,50 @@ cong_algorithm = 'cubic'
 expected_packet_per_sec = bandwidth / (length_packet << 3)
 sleeptime = 1.0 / expected_packet_per_sec
 prev_sleeptime = sleeptime
-pcap_path = "/home/wmnlab/D/pcap_data2"
+pcap_path = "/home/wmnlab/D/pcap_data"
 ss_dir = "/home/wmnlab/D/ss"
 hostname = str(PORT) + ":"
 
 cong = 'reno'.encode()
+exit_program = False
 
-# tcpproc1 =  subprocess.Popen(["tcpdump -i any port %s -w %s &"%(PORT,  pcapfile1)], shell=True, preexec_fn=os.setsid)
 now = dt.datetime.today()
 n = '-'.join([str(x) for x in[ now.year, now.month, now.day, now.hour, now.minute, now.second]])
-
 pcapfile1 = "%s/UL_%s_%s.pcap"%(pcap_path, PORT, n)
 filename = "s_port_%s_running.tmp"%(PORT)
 os.system("echo \"idle\" > %s"%(filename))
-tcpproc1 =  subprocess.Popen(["tcpdump -i any port %s -w %s &"%(PORT,  pcapfile1)], shell=True, preexec_fn=os.setsid)
-socket_proc =  subprocess.Popen(["./server.o %s&"%(PORT)], shell=True, preexec_fn=os.setsid)
-print(socket_proc.pid)
+tcpproc1 =  subprocess.Popen(["sudo tcpdump -i any port %s -w %s &"%(PORT,  pcapfile1)], shell=True, preexec_fn=os.setsid)
+socket_proc =  subprocess.Popen(["./server.o %s"%(PORT)], shell=True)
+
 while True:
     try:
+        time.sleep(1)
         f = open(filename, 'r')
         l = f.readline()
         f.close()
         if "FINISH" in l:
             pid = l.split(" ")[1] # FINISH "PID"
+            os.system("sudo kill -9 " + pid)
+            
             os.killpg(os.getpgid(tcpproc1.pid), signal.SIGTERM)
             break
-        time.sleep(1)
+        elif "FAIL" in l:
+            print("FAIL")
+            pid = l.split(" ")[1] # FINISH "PID"
+            os.system("sudo kill -9 " + pid)
+            os.killpg(os.getpgid(tcpproc1.pid), signal.SIGTERM)
+            break
+
     except KeyboardInterrupt:
         pid = l.split(" ")[1] # FINISH "PID"
-        os.system("kill -9 " + pid)
+        os.system("sudo kill -9 " + pid)
         os.killpg(os.getpgid(socket_proc.pid), signal.SIGTERM)
         os.killpg(os.getpgid(tcpproc1.pid), signal.SIGTERM)
+        exit_program = True
         break
     except Exception as e:
         print("error", e)
+        break
 
-subprocess.Popen(["rm %s"%(filename)], shell=True)
+os.system("sudo killall -9 " + "server.o")
 os.killpg(os.getpgid(tcpproc1.pid), signal.SIGTERM)
--- mptcp_auto_server_r.py
+++ mptcp_auto_server_r.py
@@ -22,13 +22,13 @@ PORT = args.port1           # UL
 thread_stop = False
 exit_program = False
 length_packet = 362
-bandwidth = 2896
+bandwidth = 289
 total_time = 3600
 cong_algorithm = 'cubic'
 expected_packet_per_sec = bandwidth / (length_packet << 3)
 sleeptime = 1.0 / expected_packet_per_sec
 prev_sleeptime = sleeptime
-pcap_path = "/home/wmnlab/D/pcap_data2"
+pcap_path = "/home/wmnlab/D/pcap_data"
 ss_dir = "/home/wmnlab/D/ss"
 hostname = str(PORT) + ":"
 
@@ -43,17 +43,27 @@ os.system("echo \"idle\" > %s"%(filename))
 tcpproc1 =  subprocess.Popen(["tcpdump -i any port %s -w %s &"%(PORT,  pcapfile1)], shell=True, preexec_fn=os.setsid)
 socket_proc =  subprocess.Popen(["./server_r.o %s %s %s&"%(PORT, bandwidth, length_packet)], shell=True, preexec_fn=os.setsid)
 print(socket_proc.pid)
+time.sleep(1)
+
 while True:
     try:
+        time.sleep(1)
         f = open(filename, 'r')
         l = f.readline()
         f.close()
-        if "FINISH" in l or "FAIL" in l:
+
+        if "FINISH" in l:
             pid = l.split(" ")[1] # FINISH "PID"
-            os.system("kill -9 " + pid)
+            os.system("sudo kill -9 " + pid)
             os.killpg(os.getpgid(tcpproc1.pid), signal.SIGTERM)
             break
-        time.sleep(1)
+        elif "FAIL" in l:
+            print("FAIL")
+            pid = l.split(" ")[1] # FINISH "PID"
+            os.system("sudo kill -9 " + pid)
+            os.killpg(os.getpgid(tcpproc1.pid), signal.SIGTERM)
+            break
+
     except KeyboardInterrupt:
         pid = l.split(" ")[1] # FINISH "PID"
         os.killpg(os.getpgid(socket_proc.pid), signal.SIGTERM)
@@ -61,6 +71,8 @@ while True:
         break
     except Exception as e:
         print("error", e)
-        
+
+
+os.system("sudo killall -9 server_r.o")
 subprocess.Popen(["rm %s"%(filename)], shell=True)
 os.killpg(os.getpgid(tcpproc1.pid), signal.SIGTERM)

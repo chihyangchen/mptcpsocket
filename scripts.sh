e946d1f7c6a0f998b18dd9d46ad91a1589a51b14
JingYou <ken32293355@gmail.com>
2022-05-07 18:16:12 +0800

    .

--- scripts.sh
+++ scripts.sh
@@ -3,3 +3,4 @@ dmesg | grep MPTCP
 sudo sysctl -w net.mptcp.mptcp_scheduler=redundant
 sudo sysctl -w net.mptcp.mptcp_scheduler=default
 sudo sysctl -w net.mptcp.mptcp_enabled=1
+sudo sysctl -a | grep mptcp

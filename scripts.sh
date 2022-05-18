97339b0279eb66cfb3f9908222acedb996536797
JingYou <ken32293355@gmail.com>
2022-05-18 18:26:47 +0800

    .

--- scripts.sh
+++ scripts.sh
@@ -5,3 +5,12 @@ sudo sysctl -w net.mptcp.mptcp_scheduler=default
 sudo sysctl -w net.mptcp.mptcp_scheduler=blest
 sudo sysctl -w net.mptcp.mptcp_enabled=1
 sudo sysctl -a | grep mptcp
+
+
+
+
+echo reno > /proc/sys/net/ipv4/tcp_congestion_control
+echo cubic > /proc/sys/net/ipv4/tcp_congestion_control
+echo bbr > /proc/sys/net/ipv4/tcp_congestion_control
+
+cat /proc/sys/net/ipv4/tcp_congestion_control

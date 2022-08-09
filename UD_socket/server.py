18ad5f9d22376a8141f590cdefbe9f3c8c737136
Jing You <ken32293355@gmail.com>
2022-08-09 16:36:56 +0800

    .

--- UD_socket/server.py
+++ UD_socket/server.py
@@ -8,9 +8,9 @@
     This script will create two sockets, UL and DL.
     You could change the PARAMETERS below.
     Run:
-        $ python3 server.py -p Port -H server_ip_address
+        $ python3 server.py -p Port
     ex:
-        $ python3 server.py -p 3270 -d 140.112.20.183
+        $ python3 server.py -p 3270
 
 """
 
